0b19a01a557c5ec2dff12567c68501a10f2a703a
Denis Lachance <dudeofea@hotmail.com>
2015-11-14 20:17:21 -0700

    added baren front page with 2 buttons

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'anonument'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -31,55 +31,35 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // Each state's controller can be found in controllers.js
   $stateProvider
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
+  // setup an abstract state for the anonument directive
+    .state('anonument', {
+    url: '/anonument',
     abstract: true,
-    templateUrl: 'templates/tabs.html'
+    templateUrl: 'templates/main.html'
   })
 
   // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
+  .state('anonument.home', {
+    url: '/home',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
+      'main-view': {
+        templateUrl: 'templates/home.html'
       }
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
+  .state('anonument.create', {
+      url: '/create',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+        'main-view': {
+          templateUrl: 'templates/create.html',
+          controller: 'CreateCtrl'
         }
       }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/anonument/home');
 
 });
