a844ca938b6097b53587d20633ee5b2f53973457
Denis Lachance <dudeofea@hotmail.com>
2015-12-20 18:28:37 -0700

    added color sliders

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,9 @@
 // Ionic Starter App
 
+//Init Parse Keys
+Parse.initialize('yuRBoDhzewR97zctSJeKBZWdv3UFeiPTt7y3zvoJ',		//Application ID
+				 'K0qtZ0NsTuhoTM22k0otm29UxBUjxJzncFmxvJwR');		//JS key
+
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
